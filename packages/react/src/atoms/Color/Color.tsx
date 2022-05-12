e3557da2f6e0d7a8e8f17212724d59b9cc24d5cc
Samer Kayali <samerkkr@gmail.com>
2022-05-11 21:46:12 -0300

    feat: added Color atom

---
+++ packages/react/src/atoms/Color/Color.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import  Spacing  from '../../foundation'
+
+export interface ColorProps {
+    hexCode: string
+    width?: keyof typeof Spacing,
+    height?: keyof typeof Spacing
+}
+
+const Color: React.FC<ColorProps> = ({ hexCode, width = Spacing.sm, height = Spacing.sm }) => {
+    const className = `dse-width-${width} dse-height-${height}`
+
+    return <div className={className} style={{
+        backgroundColor: hexCode,
+    }}>
+
+    </div>
+}
+
+export default Color
--- playgrounds/react/src/index.tsx
+++ playgrounds/react/src/index.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { Button } from "@ds/react";
-import "@ds/scss/lib/Button.css";
-ReactDOM.render(<Button label="samin" />, document.querySelector("#root"));
+import { Color, Spacing } from "@ds/react";
+import "@ds/scss/lib/Utilities.css";
+
+ReactDOM.render(
+  <Color hexCode="#000" height={Spacing.lg} width={Spacing.lg} />,
+  document.querySelector("#root")
+);
