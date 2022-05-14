e956d2723ea6c57b5498b13ecfc094e726c896db
Samer Kayali <samerkkr@gmail.com>
2022-05-13 21:03:36 -0300

    created margin component

---
+++ packages/react/src/atoms/Margin/Margin.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Spacing } from "@ds/foundation";
+
+export interface MarginProps {
+  space?: keyof typeof Spacing;
+  left?: boolean;
+  right?: boolean;
+  top?: boolean;
+  bottom?: boolean;
+  children?: React.ReactNode | React.ReactNode[];
+}
+
+const Margin = ({
+  space = "xxxs",
+  children,
+  left,
+  right,
+  top,
+  bottom,
+}: MarginProps) => {
+  let className = ``;
+
+  if (!left && !right && !top && !bottom) {
+    className = `dse-margin-${space}`;
+  }
+
+  if (left) {
+    className = `${className} dse-margin-left-${space}`;
+  }
+
+  if (right) {
+    className = `${className} dse-margin-right-${space}`;
+  }
+
+  if (top) {
+    className = `${className} dse-margin-top-${space}`;
+  }
+
+  if (bottom) {
+    className = `${className} dse-margin-bottom-${space}`;
+  }
+
+  return <div className={className}>{children}</div>;
+};
+
+export default Margin;
--- playgrounds/react/src/index.tsx
+++ playgrounds/react/src/index.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { Color, Text } from "@ds/react";
+import { Color, Text, Margin } from "@ds/react";
 import "@ds/scss/lib/Utilities.css";
 import "@ds/scss/lib/Text.css";
+import "@ds/scss/lib/Margin.css";
 
 
 ReactDOM.render(
   <>
     <Color hexCode="#000" />
+    <Margin left={true} space="xxl">
+      Samer
+    </Margin>
     <Text size="xl">Test</Text>
   </>,
   document.querySelector("#root")
