fc1a2fce0b131b51d28ebdbd276ee2b88d7de934
Samer Kayali <samerkkr@gmail.com>
2022-05-31 21:51:26 -0300

    feat: added stories to Select

---
+++ packages/react/src/molecules/Select/Select.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Select from './Select'
+
+// css
+import '@ds/scss/lib/Select.css'
+
+const options = [{
+    label: 'Strict Black',
+    value: 'black'
+}, {
+    label: 'Heavenly Green',
+    value: 'green'
+}, {
+    label: 'Sweet Pink',
+    value: 'pink'
+}]
+
+export default {
+    title: 'Molecules|Select',
+}
+
+export const Common = () => <Select options={options} />
+
+export const RenderOption = () =>
+    <Select options={options} renderOption={({ getOptionRecommendedProps, option, isSelected }) => <span {...getOptionRecommendedProps()}>{option.label} {isSelected ? 'SELECTED !' : ''}</span>} />
+
+export const CustomLabel = () => <Select label='Select a color' options={options} />
